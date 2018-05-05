df4904fea19ace71e03e29bbad2d3c8a7c54562c
alice <bow1226@gmail.com>
2018-05-05 17:42:58 +0800

    Convert start position to current position rather than map center
    Convert start position to current position rather than map center

--- motion_planning.py
+++ motion_planning.py
@@ -149,7 +149,9 @@ class MotionPlanning(Drone):
         grid_start_north = int(np.ceil(north_offset - north_offset))
         
         # TODO: convert start position to current position rather than map center
-
+        goal_north, goal_east, goal_alt = global_to_local(self.goal_global_position, self.global_home)
+        grid_goal = (int(np.ceil(goal_north - north_offset)), int(np.ceil(goal_east - east_offset)))
+        
         # Set goal as some arbitrary position on the grid
         grid_goal = (-north_offset + 10, -east_offset + 10)
         
