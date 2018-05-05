6218d788b4eff0d2f7cc4575010f857b10d9de31
alice <bow1226@gmail.com>
2018-05-05 18:19:05 +0800

    Add prune_path function and point func
    Add prune_path function and point func.

--- planning_utils.py
+++ planning_utils.py
@@ -148,16 +148,38 @@ def read_lat_lon(filename):
         lat, lon = float(latitude.split(' ')[-1]), float(longitude.split(' ')[-1])    
 
     return lon, lat
-    
+
+def point(p):
+    return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
 def collinearity_check(p1, p2, p3, epsilon=1e-6):   
     m = np.concatenate((p1, p2, p3), 0)
     det = np.linalg.det(m)
     return True if abs(det) < epsilon else False
 
 def prune_path(path):
-    
+    pruned_path = [p for p in path]
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
 
-    return path
+        # If the 3 points are in a line remove
+        # the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_check(p1, p2, p3, 2.618):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            # for p in pruned_path[i+1:i+2*step-1]:
+            #     pruned_path.remove(p)
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path 
 
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
