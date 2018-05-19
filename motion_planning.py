11982aca56e43a0b0a8c67955db53d4b0d300a84
alice <bow1226@gmail.com>
2018-05-19 11:51:21 +0800

    Fix goal to (287, 675)

--- motion_planning.py
+++ motion_planning.py
@@ -155,8 +155,8 @@ class MotionPlanning(Drone):
         
         # Set goal as some arbitrary position on the grid  
         # TODO: adapt to set goal as latitude / longitude position and convert
-        grid_goal = (720, 30)
-        
+        grid_goal = (287,675)
+
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
